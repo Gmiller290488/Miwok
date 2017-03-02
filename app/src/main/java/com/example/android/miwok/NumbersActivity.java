88662138ce1000808cddc1121b8f5ef98b11c047
Gareth <gmiller290488@gmail.com>
2017-03-02 14:29:36 +0000

    Lesson 2: ListView and adapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,8 +17,9 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -42,11 +43,10 @@ public class NumbersActivity extends AppCompatActivity {
 
 
         LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        for (int index = 0; index < 10; index++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
 
     }
