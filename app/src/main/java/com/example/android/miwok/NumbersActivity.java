694babf8f88740a04752a048d1cab087cbd47ef3
Gareth <gmiller290488@gmail.com>
2017-03-03 10:09:43 +0000

    add a new constructor + add images to objects

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,16 +15,16 @@ public class NumbersActivity extends AppCompatActivity {
 
         // Create a list of words
         ArrayList<word> words = new ArrayList<word>();
-        words.add(new word("one", "lutti"));
-        words.add(new word("two", "otiiko"));
-        words.add(new word("three", "tolookosu"));
-        words.add(new word("four", "oyyisa"));
-        words.add(new word("five", "massokka"));
-        words.add(new word("six", "temmokka"));
-        words.add(new word("seven", "kenekaku"));
-        words.add(new word("eight", "kawinta"));
-        words.add(new word("nine", "wo’e"));
-        words.add(new word("ten", "na’aacha"));
+        words.add(new word("one", "lutti", R.drawable.number_one));
+        words.add(new word("two", "otiiko", R.drawable.number_two));
+        words.add(new word("three", "tolookosu", R.drawable.number_three));
+        words.add(new word("four", "oyyisa", R.drawable.number_four));
+        words.add(new word("five", "massokka", R.drawable.number_five));
+        words.add(new word("six", "temmokka", R.drawable.number_six));
+        words.add(new word("seven", "kenekaku", R.drawable.number_seven));
+        words.add(new word("eight", "kawinta", R.drawable.number_eight));
+        words.add(new word("nine", "wo’e", R.drawable.number_nine));
+        words.add(new word("ten", "na’aacha", R.drawable.number_ten));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
