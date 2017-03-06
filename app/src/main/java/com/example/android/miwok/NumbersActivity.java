b8db2c33e74f2569539fdf3f053ad32a7667a4a3
Gareth <gmiller290488@gmail.com>
2017-03-06 13:38:08 +0000

    added audio for each word

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,30 +1,56 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
 
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
+
+
+
         // Create a list of words
-        ArrayList<word> words = new ArrayList<word>();
-        words.add(new word("one", "lutti", R.drawable.number_one));
-        words.add(new word("two", "otiiko", R.drawable.number_two));
-        words.add(new word("three", "tolookosu", R.drawable.number_three));
-        words.add(new word("four", "oyyisa", R.drawable.number_four));
-        words.add(new word("five", "massokka", R.drawable.number_five));
-        words.add(new word("six", "temmokka", R.drawable.number_six));
-        words.add(new word("seven", "kenekaku", R.drawable.number_seven));
-        words.add(new word("eight", "kawinta", R.drawable.number_eight));
-        words.add(new word("nine", "wo’e", R.drawable.number_nine));
-        words.add(new word("ten", "na’aacha", R.drawable.number_ten));
+        final ArrayList<word> words = new ArrayList<word>();
+        words.add(new word("one", "lutti", R.drawable.number_one, R.raw.number_one));
+        words.add(new word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new word("five", "massokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_ten));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
@@ -35,8 +61,22 @@ public class NumbersActivity extends AppCompatActivity {
         // word_list.xml file.
         ListView listView = (ListView) findViewById(R.id.list);
 
+
+
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
         // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                word word = words.get(position);
+                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceID());
+                releaseMediaPlayer();
+                mMediaPlayer.start();
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -15,30 +15,54 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<word> words = new ArrayList<word>();
-        words.add(new word("Where are you going?", "minto wuksus"));
-        words.add(new word("What is your name", "tinnә oyaase'nә"));
-        words.add(new word("My name is...", "oyaaset..."));
-        words.add(new word("How are you feeling?", "michәksәs?"));
-        words.add(new word("I'm feeling good.", "kuchi achit"));
-        words.add(new word("Are you coming?", "әәnәs'aa?"));
-        words.add(new word("Yes, I'm coming", "hәә’ әәnәm"));
-        words.add(new word("I'm coming.", "әәnәm"));
-        words.add(new word("Let's go.", "yoowutis"));
-        words.add(new word("Come here.", "әnni'nem"));
+        final ArrayList<word> words = new ArrayList<word>();
+        words.add(new word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
+        words.add(new word("What is your name", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
+        words.add(new word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
+        words.add(new word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
+        words.add(new word("I'm feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
+        words.add(new word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
+        words.add(new word("Yes, I'm coming", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
+        words.add(new word("I'm coming.", "әәnәm", R.raw.phrase_im_coming));
+        words.add(new word("Let's go.", "yoowutis", R.raw.phrase_lets_go));
+        words.add(new word("Come here.", "әnni'nem", R.raw.phrase_come_here));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
@@ -53,6 +77,16 @@ public class PhrasesActivity extends AppCompatActivity {
         // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
 
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                word word = words.get(position);
+                mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getAudioResourceID());
+                releaseMediaPlayer();
+                mMediaPlayer.start();
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+            }
+        });
     }
 }
